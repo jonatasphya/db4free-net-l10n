4da906c4610bd52f0d83fc4cc9aff86ae6458e9d
jonatasphya <jonatasphya@gmail.com>
2014-10-26 16:02:03 -0300

    Create about.php

---
+++ ptbr/about.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Section: about
+Language: Português Brasil
+*/
+$translations = array(
+'h1' => 'Sobre o projeto db4free.net',
+'p1' => 'Nesta seção você pode criar sua conta de banco de dados, modifique dados e apague.',
+'h2' => 'O que nos torna "Especial"?',
+'p2' => 'O objetivo do nosso projeto é a intenção de satisfazer as necessidades de desenvolvedores e testadores para<b>versões atuais</b> de MYSQL. Ao atualizar as versões do MySQL e phpMyAdmin com freqüência, você tem, por exemplo, a oportunidade de testar o seu site com uma versão atual do MySQL.',
+'h3' => '<a href="signup.php">Tenha seu banco de dados MYSQL grátis&nbsp;&raquo;</a>',
+);
+?>
